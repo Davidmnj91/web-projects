cb49d143313ba0f44a353c253967621edf815fd0
DavidMnj91 <david.manja91@gmail.com>
2025-11-12 22:17:47 +0100

    feat: monorepo setup

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import { configs, defineConfig } from '@web-projects/eslint'
+
+export default defineConfig(
+  {
+    ignores: [
+      'apps',
+      'libs',
+      'config',
+      'tools',
+      'prettier.config.js',
+      'lint-staged.config.js',
+      'commitlint.config.js',
+      ...configs.base.flatMap((c) => c.ignores ?? []),
+    ],
+  },
+  ...configs.base,
+)
