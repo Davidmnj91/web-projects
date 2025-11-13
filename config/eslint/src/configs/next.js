93d3c0c0afd0c6d8860e6737014e91cbcc4c2bab
DavidMnj91 <david.manja91@gmail.com>
2025-11-13 22:59:58 +0100

    chore: cleanup

--- config/eslint/src/configs/next.js
+++ config/eslint/src/configs/next.js
@@ -1,32 +1,14 @@
-import { fixupConfigRules } from '@eslint/compat'
 import eslintPluginNext from '@next/eslint-plugin-next'
-import reactRefresh from 'eslint-plugin-react-refresh'
 
-import { compat, defineConfig } from '../utils.js'
+import { defineConfig } from '../utils.js'
 import { base } from './base.js'
+import { react } from './react.js'
 
 export const next = defineConfig(
+  ...react,
   {
     ignores: ['.next', 'postcss.config.mjs', ...base.flatMap((c) => c.ignores ?? [])],
   },
   eslintPluginNext.configs.recommended,
   eslintPluginNext.configs['core-web-vitals'],
-  ...fixupConfigRules(compat.extends('plugin:react/recommended')),
-  ...fixupConfigRules(compat.extends('plugin:react-hooks/recommended')),
-  ...fixupConfigRules(compat.extends('plugin:jsx-a11y/strict')),
-
-  {
-    plugins: {
-      'react-refresh': reactRefresh,
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-    },
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-    },
-  },
 )
