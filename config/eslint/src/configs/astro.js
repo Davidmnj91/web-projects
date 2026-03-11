f92a7c30d0574026c26430d2bcf401d27ba55930
DavidMnj91 <david.manja91@gmail.com>
2026-03-11 22:01:06 +0100

    feat: new ozz website project

--- config/eslint/src/configs/astro.js
+++ config/eslint/src/configs/astro.js
@@ -2,4 +2,22 @@ import * as eslintPluginAstro from 'eslint-plugin-astro'
 
 import { defineConfig } from '../utils.js'
 
-export const astro = defineConfig(...eslintPluginAstro.configs.recommended, { ignores: ['.astro'] })
+export const astro = defineConfig(
+  ...(Array.isArray(eslintPluginAstro.configs.recommended)
+    ? eslintPluginAstro.configs.recommended
+    : [eslintPluginAstro.configs.recommended]),
+  {
+    files: ['**/*.astro'],
+    languageOptions: {
+      parserOptions: {
+        // Disable projectService for Astro files as it's not fully supported yet by the parser
+        projectService: false,
+        project: true,
+      },
+    },
+    rules: {
+      // Disable some type-checked rules in Astro files if they cause issues with the parser
+      '@typescript-eslint/no-unsafe-return': 'off',
+    },
+  },
+)
