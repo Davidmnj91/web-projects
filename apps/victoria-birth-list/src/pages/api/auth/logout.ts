88a0366f06cc8684622af45f92eaef08c53d5e8f
DavidMnj91 <david.manja91@gmail.com>
2026-05-30 00:01:00 +0200

    chore: move apps/victoria-birth-list to a standalone repository

--- apps/victoria-birth-list/src/pages/api/auth/logout.ts
+++
@@ -1,8 +0,0 @@
-import { cookieConfig } from '../../../lib/auth'
-
-import type { APIRoute } from 'astro'
-
-export const GET: APIRoute = ({ cookies, redirect }) => {
-  cookies.delete(cookieConfig.name, { path: '/' })
-  return redirect('/')
-}
